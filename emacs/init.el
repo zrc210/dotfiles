7b8036247c17c24fefbbf9150e59b8f3345bb388
zrc210 <zubair@chaudary.com>
2016-06-01 22:32:45 -0400

    Better Files

--- emacs/init.el
+++ emacs/init.el
@@ -1,17 +1,15 @@
-(add-to-list 'custom-theme-load-path "~/git/emacs-color-theme-solarized/")
-(load-theme 'solarized t)
-(set-frame-parameter nil 'background-mode 'dark)
-(set-terminal-parameter nil 'background-mode 'dark)
-(enable-theme 'solarized)
-
-(add-to-list 'load-path "~/git/enhanced-ruby-mode") ; must be added after any path containing old ruby-mode
-(autoload 'enh-ruby-mode "enh-ruby-mode" "Major mode for ruby files" t)
-(add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
-(add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
+(prelude-require-packages '(enh-ruby-mode robe handlebars-mode yaml-mode web-mode))
 
 (add-hook 'text-mode-hook 'flyspell-mode)
 (add-hook 'prog-mode-hook 'flyspell-prog-mode)
 
-(require 'package)
-(add-to-list 'package-archives
-	     '("melpa" . "http://stable.melpa.org/packages/") t)
+;; [tab] inserts two spaces
+(global-set-key "\t" (lambda () (interactive) (insert-char 32 2)))
+
+(setq-default indent-tabs-mode nil)
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2))
+
+(add-hook 'web-mode-hook  'my-web-mode-hook)
