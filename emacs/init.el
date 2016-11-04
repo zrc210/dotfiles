795184793f3098bdee38b480aea307375e782ad2
Zubair <zubair@simulmedia.com>
2016-11-04 13:17:25 -0400

    updates

--- emacs/init.el
+++ emacs/init.el
@@ -6,6 +6,8 @@
 ;; [tab] inserts two spaces
 (global-set-key "\t" (lambda () (interactive) (insert-char 32 2)))
 
+(global-linum-mode t)
+
 (setq-default indent-tabs-mode nil)
 
 (defun my-web-mode-hook ()
@@ -19,3 +21,5 @@
 (setq tab-width 2)
 
 (smart-tabs-insinuate 'ruby 'javascript)
+
+(setq linum-format "%d ")
